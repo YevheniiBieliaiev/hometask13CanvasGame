9dbbde538728b540c9c7ba7579937506e94054d8
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2021-08-04 15:57:51 +0300

    asPerAdvices

--- js/main.js
+++ js/main.js
@@ -1,74 +1,76 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
+//сторона квадрата
+let squareSide = 20;
+//переменные для случайной скорости
+let minSpeed = 1; // для целого числа
+let maxSpeed = 4;// для целого числа
+//переменные для случайного интервала
+let minTime = 200;
+let maxTime = 3000;
+//для определения координаты X
+let startX = 0;
+let endX = canvas.width - squareSide;
+//для цвета
+let starColor = 0;
+let endColor = 255;
+//функция для рандомного числа (в диапазоне чисел) - цвет,скорость,координата Х,интервал времени
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//функция для рандомного цвета
+function randomColor() {
+  return "rgb(" + randomNumber(starColor, endColor) + "," + randomNumber(starColor, endColor) + "," + randomNumber(starColor, endColor) + ")";
+}
+//массив для хранения объектов 
+let squares = [];
+
+
 function animate() {
-  //значение для случайного цвета
-  function randomColor() {
-    function randNumb() {
-      return Math.floor(Math.random() * (255 + 1));
-    }
-    return "rgb(" + randNumb() + "," + randNumb() + "," + randNumb() + ")";
-  }
-  //случайная координата Х
-  function randomX() {
-    return Math.floor(Math.random() * (canvas.width - 20));
-  }
-  //случайная скорость
-  let minSpeed = 1; // для целого числа
-  let maxSpeed = 4;// для целого числа
-  function randomSpeed() {
-    return Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + minSpeed;
-  }
-  //случайный интервал времени
-  function randomInterval() {
-    let minTime = 200;
-    let maxTime = 3000;
-    return Math.floor(Math.random() * (maxTime - minTime + 1)) + minTime;
-  }
-  //стоп рисования и очистка поля
-  function cleaner(variable) {
-    clearInterval(variable);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
-  //массив для хранения объектов 
-  let squareArr = [];
-  //объек с параметрами
-  const Square = {
-    x: randomX(),
-    y: 0,
-    squareSide: 20,
-    color: randomColor(),
-    ySpeed: randomSpeed()
-  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   //рисование квадрата
   function drawSquare(obj) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.rect(obj.x, obj.y, obj.squareSide, obj.squareSide);
     ctx.fillStyle = obj.color;
     ctx.fill();
     ctx.closePath();
   }
-
-
   function draw() {
-    drawSquare(Square);
-
-
+    for (let key of squares) {
+      drawSquare(key);
+    }
   }
+  draw();
 
-
-  let timeID;
-  document.querySelector(".stop").addEventListener("click", event => {
-    if (event.target) { cleaner(timeID); }
-  });
-  document.querySelector(".start").addEventListener("click", event => {
-    if (event.target) { timeID = setInterval(draw, randomInterval()); }
-  });
-  //requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 }
 
-
+let timeID;
+//старт рисования
+document.querySelector(".start").addEventListener("click", event => {
+  if (event.target) {
+    timeID = setInterval(function () {
+      const square = {
+        x: randomNumber(startX, endColor),
+        y: 0,
+        squareSide: squareSide,
+        color: randomColor(),
+        ySpeed: randomNumber(minSpeed, maxSpeed)
+      };
+      squares.push(square);
+      animate();
+    }, randomNumber(minTime, maxTime));
+  }
+});
+//остановка и очистка поля
+document.querySelector(".stop").addEventListener("click", event => {
+  if (event.target) { clearInterval(timeID); }
+});
 
 
 canvas.addEventListener('click', (event) => { });
